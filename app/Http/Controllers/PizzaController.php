5111369f47b7bc43618950f3b84aba2708442bf4
chiefJurist <anthonynnanna004@gmail.com>
2023-10-25 05:56:23 +0100

    another commit

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -44,4 +44,11 @@ class PizzaController extends Controller
 
         return redirect('/')->with('mssg', 'Thanks for your order');
     }
+
+    public function destroy ($id) {
+        $pizza = Pizza::findOrFail($id);
+        $pizza->delete();
+
+        return redirect('/pizzas');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/pizzas', 'App\Http\Controllers\PizzaController@index');
 Route::get('/pizzas/create', 'App\Http\Controllers\PizzaController@create');
 Route::post('/pizzas','App\Http\Controllers\PizzaController@store');
 Route::get('/pizzas/{id}', 'App\Http\Controllers\PizzaController@show');
+Route::delete('/pizzas/{id}', 'App\Http\Controllers\PizzaController@destroy');

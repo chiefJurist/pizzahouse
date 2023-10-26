d596af073acc652419f3fbec248a94c4ad5d2197
chiefJurist <anthonynnanna004@gmail.com>
2023-10-26 05:14:41 +0100

    another commit

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ Route::get('/pizzas/create', 'App\Http\Controllers\PizzaController@create');
 Route::post('/pizzas','App\Http\Controllers\PizzaController@store');
 Route::get('/pizzas/{id}', 'App\Http\Controllers\PizzaController@show')->middleware('auth');
 Route::delete('/pizzas/{id}', 'App\Http\Controllers\PizzaController@destroy')->middleware('auth');
-Auth::routes();
+
+
+Auth::routes([
+    'register'=> false
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
